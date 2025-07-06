c7a887e34623377ef26eb2387ff69b03bce65616
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2025-07-07 00:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
+
+
+const list = document.getElementById("list");
+
+function cleanTitle(title) {
+  return title.replace(/^(a |an |the )/i, "").trim();
+}
+
+bands.sort((a, b) => {
+  const titleA = cleanTitle(a).toLowerCase();
+  const titleB = cleanTitle(b).toLowerCase();
+  return titleA.localeCompare(titleB);
+});
+bands.forEach((article) => {
+  const li = document.createElement("li");
+  li.textContent = article;
+  list.appendChild(li);
+});
